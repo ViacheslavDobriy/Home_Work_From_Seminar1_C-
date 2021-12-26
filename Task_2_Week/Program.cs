a35ecfbc8fafb26731b4ba1daa4b97bf0cdab3c6
Vyacheslav <dobrov.slava@yandex.ru>
2021-12-26 22:04:31 +0300

    Added new task 2, it will be work if user enter numbers from 1 till 7

---
+++ Task_2_Week/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello! You are welcome in programm Day of the Week");
+Console.WriteLine("Enter day's number of the week");
+string Number=Console.ReadLine();
+string Days_Name="";
+if(Number=="1"){
+    Days_Name="Monday";
+}
+if(Number=="2"){
+    Days_Name="Tuesday";
+}
+if(Number=="3"){
+    Days_Name="Wednesday";
+}
+if(Number=="4"){
+    Days_Name="Thursday";
+}
+if(Number=="5"){
+    Days_Name="Friday";
+}
+if(Number=="6"){
+    Days_Name="Saturday";
+}
+if(Number=="7"){
+    Days_Name="Sunday";
+}
+Console.Write("Today is the ");
+Console.WriteLine(Days_Name);
