1f049d6c3a671a26ae5c77570995ecb9148da430
Vyacheslav <dobrov.slava@yandex.ru>
2021-12-27 00:33:46 +0300

    Added new programm with using remains

---
+++ Task_8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("I have number 128. Try to enter any number and I'll show you if your number is multiple to mine or not");
+int User_Number = Convert.ToInt32(Console.ReadLine());
+int Remains = 128 % User_Number;
+if (Remains==0){
+    Console.WriteLine("Your number is multiple to 128");
+}
+else {Console.WriteLine($"Your number is not multiple to 128, remains is {Remains}");
+}
