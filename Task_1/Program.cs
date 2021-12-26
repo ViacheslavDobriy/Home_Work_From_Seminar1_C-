62a701cf8b38590022bc9b23fcb2b0cb7da8d36d
Vyacheslav <dobrov.slava@yandex.ru>
2021-12-26 18:51:43 +0300

    Changed programm Heelo World to Hello User

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter first name");
+string First_Number=Console.ReadLine();
+Console.WriteLine("Enter second name");
+string Second_Number=Console.ReadLine();
+Console.Write(First_Number);
+Console.Write(" ");
+Console.Write(Second_Number);
