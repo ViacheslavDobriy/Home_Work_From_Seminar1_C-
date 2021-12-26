514eb44c19486d879c713246d29dfbb3031fbc8b
Vyacheslav <dobrov.slava@yandex.ru>
2021-12-26 22:15:00 +0300

    Now programm work ever if you entered illegal number

--- Task_2_Week/Program.cs
+++ Task_2_Week/Program.cs
@@ -4,24 +4,37 @@ string Number=Console.ReadLine();
 string Days_Name="";
 if(Number=="1"){
     Days_Name="Monday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-if(Number=="2"){
+else if(Number=="2"){
     Days_Name="Tuesday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-if(Number=="3"){
+else if(Number=="3"){
     Days_Name="Wednesday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-if(Number=="4"){
+else if(Number=="4"){
     Days_Name="Thursday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-if(Number=="5"){
+else if(Number=="5"){
     Days_Name="Friday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-if(Number=="6"){
+else if(Number=="6"){
     Days_Name="Saturday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-if(Number=="7"){
+else if(Number=="7"){
     Days_Name="Sunday";
+    Console.Write("Today is the ");
+    Console.WriteLine(Days_Name);
 }
-Console.Write("Today is the ");
-Console.WriteLine(Days_Name);
+else {Console.WriteLine("You entered illegal number");}
