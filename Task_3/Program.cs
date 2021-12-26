71a7745a6fa84c3568ecfa9aab51314b523fb761
Vyacheslav <dobrov.slava@yandex.ru>
2021-12-26 23:01:08 +0300

    Added programm with view from -N till N

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, User! Enter any integer number, and I will show you segment of numbers from -Your_Number till +Your_Number");
+int number = Convert.ToInt32(Console.ReadLine());
+int Updated_number=number*(-1);
+while(Updated_number<number+1){
+    Console.Write(" ");
+    Console.Write(Updated_number);
+    Updated_number++;
+}
+Console.WriteLine();
+Console.WriteLine("Done! Look at this");
