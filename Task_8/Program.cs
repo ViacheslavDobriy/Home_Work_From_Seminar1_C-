495b2688281622f353628ee0c81e66d243bac565
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-01 22:50:41 +0300

    Написал задание к 8 задаче и добавил ввод еще одного числа

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("I have number 128. Try to enter any number and I'll show you if your number is multiple to mine or not");
-int User_Number = Convert.ToInt32(Console.ReadLine());
-int Remains = 128 % User_Number;
+﻿// Кратно ли одно заданное число, введенному числу
+Console.WriteLine("Задайте число, относительно которого будем проверять кратность введенного числа");
+int firstUserNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, которое будем проверять на кратность");
+int secondUserNumber = Convert.ToInt32(Console.ReadLine());
+int Remains = firstUserNumber % secondUserNumber;
 if (Remains==0){
-    Console.WriteLine("Your number is multiple to 128");
+    Console.WriteLine("{0} кратно {1}", secondUserNumber, firstUserNumber);
 }
-else {Console.WriteLine($"Your number is not multiple to 128, remains is {Remains}");
+else {Console.WriteLine($"{secondUserNumber} не кратно {firstUserNumber}, остаток от деления равен {Remains}");
 }
