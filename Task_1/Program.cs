f0504d03ec3788eae519560d4c21a968ef5cefae
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-02 15:23:34 +0300

    Добавил метод, который проверяет является ли 1 число квадратом второго

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,13 +4,17 @@ int SquareOfNumber(int number1)
     int Result = number1*number1;
     return Result;
 }
+void CheckOfSquare(int number1, int number2)
+{
+    if(number1 == number2)
+    Console.WriteLine("The first number is the square of the second");
+else 
+   Console.WriteLine("The first number isn't the square of the second");
+}
 Console.Write("Enter first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter second number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int number3 = SquareOfNumber(number2);
-if(number1 == number3){
-    Console.WriteLine("The first number is the square of the second");
-}
-else 
-   Console.WriteLine("The first number isn't the square of the second");
+int square = SquareOfNumber(number2);
+CheckOfSquare(number1,square);
+
