e3e7e5c1fe3d407e893a82d29a57495f6bb48934
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 01:22:03 +0300

    Добавил решение шестой задачи, с таким же нелепым округлением как и в пятой задаче, программа работает, также осуществена проверка на одинаковые цифры в рандомно сгенерированным числом.

--- Task_6_TheMaxDigit/Program.cs
+++ Task_6_TheMaxDigit/Program.cs
@@ -1,8 +1,23 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
-int GenerateNumber()
+double GenerateNumber()
 {
-    int number = new Random().Next(10,100);
+    double number = new Random().Next(10,100);
     return number;
 }
-int RandomNumber = GenerateNumber();
+double SeparateDigit(double WholeNumber)
+{
+    double SecondDigit = WholeNumber%10;
+    double FirstDigit = Math.Round((WholeNumber-5)/10,0);
+    if(SecondDigit>FirstDigit) return SecondDigit;
+    else if(SecondDigit<FirstDigit) return FirstDigit;
+    else 
+    {
+        Console.WriteLine("Две одинаковых цифры в двухзначном числе - {0}", FirstDigit);
+        return FirstDigit;
+    }
+}
+Console.WriteLine("Сгенерировано следующее число");
+double RandomNumber = GenerateNumber();
 Console.WriteLine(RandomNumber);
+double ResultOfChecking=SeparateDigit(RandomNumber);
+Console.WriteLine(ResultOfChecking);
