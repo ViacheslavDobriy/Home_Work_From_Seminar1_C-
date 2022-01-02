0e01f387819c1a6120f07f434db8a2bf492869f9
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-02 22:49:54 +0300

    Добавил в 3 задание две функции для ее решения, но каким-то образом произошли незначительные изменения в 1 задании, никак не повлиявшие на его корректную работу

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,10 +1,21 @@
-﻿Console.WriteLine("Hello, User! Enter any integer number, and I will show you segment of numbers from -Your_Number till +Your_Number");
-int number = Convert.ToInt32(Console.ReadLine());
-int Updated_number=number*(-1);
-while(Updated_number<number+1){
-    Console.Write(" ");
-    Console.Write(Updated_number);
-    Updated_number++;
+﻿// Программа,которая должна вывести числа от -N до N
+Console.WriteLine("Hello, User! Enter any integer number, and I will show you segment of numbers from -Your_Number till +Your_Number");
+int AlternativeNumber(int AnyNumber)
+{
+    int Result = AnyNumber*(-1);
+    return Result;
 }
+void OutPutSegment(int number, int number_)
+{
+    while(number_<number+1)
+    {
+         Console.Write(" ");
+         Console.Write(number_);
+         number_++;
+    }
+}
+int number = Convert.ToInt32(Console.ReadLine());
+int Updated_number=AlternativeNumber(number);
+OutPutSegment(number, Updated_number);
 Console.WriteLine();
 Console.WriteLine("Done! Look at this");
