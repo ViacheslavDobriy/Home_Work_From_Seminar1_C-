cd1ca9d2f259fb8e3069de358671135aff99eb9c
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-02 15:57:25 +0300

    Добавил массив типа данных string и ввел цикл for

--- Task_2_Week/Program.cs
+++ Task_2_Week/Program.cs
@@ -1,40 +1,22 @@
-﻿Console.WriteLine("Hello! You are welcome in programm Day of the Week");
+﻿// Программа, которая переводит введенный номер дня недели с клавиатуры в название дня недели
+Console.WriteLine("Hello! You are welcome in programm Day of the Week");
 Console.WriteLine("Enter day's number of the week");
-string Number=Console.ReadLine();
-string Days_Name="";
-if(Number=="1"){
-    Days_Name="Monday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
+int Number=Convert.ToInt32(Console.ReadLine());
+string[] Days_Name = new string[7];
+Days_Name[0]="Monday";
+Days_Name[1]="Tuesday";
+Days_Name[2]="Wednesday";
+Days_Name[3]="Thursday";
+Days_Name[4]="Friday";
+Days_Name[5]="Saturday";
+Days_Name[6]="Sunday";
+string ResultNameDay = " ";
+for(int i = 0; i<7; i++)
+{
+    if(Number==i+1)
+    {
+        ResultNameDay=Days_Name[i];
+        Console.Write("Today is the ");
+        Console.WriteLine(ResultNameDay);
+    }
 }
-else if(Number=="2"){
-    Days_Name="Tuesday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
-}
-else if(Number=="3"){
-    Days_Name="Wednesday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
-}
-else if(Number=="4"){
-    Days_Name="Thursday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
-}
-else if(Number=="5"){
-    Days_Name="Friday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
-}
-else if(Number=="6"){
-    Days_Name="Saturday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
-}
-else if(Number=="7"){
-    Days_Name="Sunday";
-    Console.Write("Today is the ");
-    Console.WriteLine(Days_Name);
-}
-else {Console.WriteLine("You entered illegal number");}
