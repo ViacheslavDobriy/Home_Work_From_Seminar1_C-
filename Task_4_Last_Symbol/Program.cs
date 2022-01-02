a910ccceb373c9a4bfb143affb39a9f878c8e969
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-02 23:01:57 +0300

    Добавил функцию вытаскивания последнего символа из трехзначного числа

--- Task_4_Last_Symbol/Program.cs
+++ Task_4_Last_Symbol/Program.cs
@@ -1,5 +1,13 @@
-﻿Console.WriteLine("Hello! This is my next programm, as usually enter new integer number with 3 symbols");
-string number = Console.ReadLine();
-Console.WriteLine(number);
+﻿// Программа, которая выводит 3 цифру 3-значного числа
+Console.WriteLine("Hello! This is my next programm, as usually enter new integer number with 3 symbols");
+int LastSymbolFunc(int number)
+{
+    int LastSymbol = number % 10;
+    return LastSymbol;
+}
+int number = Convert.ToInt32(Console.ReadLine());
+int Result = LastSymbolFunc(number);
+Console.WriteLine(Result);
+
 
 
