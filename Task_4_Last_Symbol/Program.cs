a958485d809bc3c0a765dff96a158b446cefa84b
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-02 23:21:54 +0300

    Добавил функцию и логику проверки на правильность ввода числа с клавиатуры пользователем

--- Task_4_Last_Symbol/Program.cs
+++ Task_4_Last_Symbol/Program.cs
@@ -5,9 +5,27 @@ int LastSymbolFunc(int number)
     int LastSymbol = number % 10;
     return LastSymbol;
 }
+bool CheckFunction(int number)
+{
+    int result = number / 100;
+    if(result >= 10)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+    }
+    else if(result < 1)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+    }
+    else return true;
+}
 int number = Convert.ToInt32(Console.ReadLine());
-int Result = LastSymbolFunc(number);
-Console.WriteLine(Result);
-
+bool ResultOfCheck = CheckFunction(number);
+if(ResultOfCheck==true)
+{
+    int Result = LastSymbolFunc(number);
+    Console.WriteLine(Result);
+}
 
 
