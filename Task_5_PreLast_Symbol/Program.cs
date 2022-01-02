1d10b9b9dc6d463015418e02eb49f3ca2285c45a
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 00:12:16 +0300

    Сделал решение пятой задачи, по своему. Не знаю, как более удачно использовать округление чисел.. Но программа работает

---
+++ Task_5_PreLast_Symbol/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать вторую цифру трёхзначного числа
+double PreLastSymbolFunc(double number)
+{
+    double PreLastSymbol = Math.Round((number - 5)/10,0)%10;
+    return PreLastSymbol;
+}
+bool CheckFunction(double number)
+{
+    double result = number / 100;
+    if(result >= 10)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+    }
+    else if(result < 1)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+    }
+    else return true;
+}
+double number = Convert.ToInt32(Console.ReadLine());
+bool ResultOfCheck = CheckFunction(number);
+if(ResultOfCheck==true)
+{
+    double Result = PreLastSymbolFunc(number);
+    Console.WriteLine(Result);
+}
