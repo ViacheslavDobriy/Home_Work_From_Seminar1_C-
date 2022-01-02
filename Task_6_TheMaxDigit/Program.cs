4779dadc173fbedb306cb97e961f4651dbc127fe
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 00:36:34 +0300

    Добавил функцию, которая генерирует число от 10 до 99

---
+++ Task_6_TheMaxDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int GenerateNumber()
+{
+    int number = new Random().Next(10,100);
+    return number;
+}
+int RandomNumber = GenerateNumber();
+Console.WriteLine(RandomNumber);
