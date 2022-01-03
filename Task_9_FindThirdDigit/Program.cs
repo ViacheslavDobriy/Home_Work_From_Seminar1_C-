5e7a22da27d5c13cd3c9ba693ba14d3da94eb0fa
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 15:20:27 +0300

    Реализовал программу, которая вытаскивает третью цифру из введенного пользователем числа, но если число меньше 100, программа выдаст сообщение об этом.

---
+++ Task_9_FindThirdDigit/Program.cs
@@ -0,0 +1,33 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+bool CheckFunction(double number)
+{
+    double result = number / 100;
+    if(result < 1)
+    {
+    Console.WriteLine("Вы ввели менее 3 цифр");
+    return false;
+    }
+    else return true;
+}
+double DeleteFunc(double number)
+{
+    double ThirdDigit = Math.Round((number-50)/100,0)%10;
+    return ThirdDigit;
+}
+double number = Convert.ToInt32(Console.ReadLine());
+bool ResultOfCheck = CheckFunction(number);
+if(ResultOfCheck==true)
+{
+    double ResultOfFunc = DeleteFunc(number);
+    if(ResultOfFunc>=10)
+    {
+        ResultOfFunc = ResultOfFunc%10;
+        Console.WriteLine(ResultOfFunc);
+    }
+    else
+    {
+        Console.WriteLine(ResultOfFunc);
+    }
+}
+
+
