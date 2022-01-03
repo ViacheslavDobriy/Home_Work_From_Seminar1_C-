0e3009721395694ceb3b48190d23828284b66a27
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-03 14:35:00 +0300

    С помощью двух функций реализовал программу, которая удаляет 2 цифру в 3-значном числе

---
+++ Task_7_DeleteSecondDigit/Program.cs
@@ -0,0 +1,30 @@
+﻿// Удалить вторую цифру трёхзначного числа
+bool CheckFunction(double number)
+{
+    double result = number / 100;
+    if(result >= 10)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+    }
+    else if(result < 1)
+    {
+    Console.WriteLine("Вы ввели не трехзначное число");
+    return false;
+    }
+    else return true;
+}
+double DeleteFunc(double number)
+{
+    double FirstDigit = Math.Round((number-50)/100,0)%10;
+    double ThirdDigit = number%10;
+    double NewNumber = FirstDigit*10 + ThirdDigit;
+    return NewNumber;
+}
+double number = Convert.ToInt32(Console.ReadLine());
+bool ResultOfCheck = CheckFunction(number);
+if(ResultOfCheck==true)
+{
+    double Result = DeleteFunc(number);
+    Console.WriteLine(Result);
+}
